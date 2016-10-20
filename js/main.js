7aa3c44351f3e174793f66bbde906a19b008e830
Eric Hagemann <hagemann.eric@gmail.com>
2016-10-20 10:14:41 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-(function() {
-  $('.target').vibrancy({
-    panelClass: 'panel',
-    backgroundSrc: $('.hidden-preload img:first-child').attr('src')
+$(function() {
+  $("nav").on("click", "li", function() {
+    var id = $(this).data("nav"),
+        $article = $("main").find("article[data-nav='" + id + "']"),
+        $all_articles = $("main").find("article"),
+        $closeBox = $article.find(".close"),
+        fade_time = 200;
+
+    $all_articles.hide();
+
+    $article.show();
+
+    $closeBox.on("click", function() {
+      $all_articles.fadeOut(fade_time);
+    });
   });
+
 });
